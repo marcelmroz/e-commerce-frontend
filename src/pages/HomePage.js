5d96ea399a097c502907e2334e72060731197945
Marcel Mroz <mmroz.marcel@gmail.com>
2023-11-11 18:00:56 +0100

    working pages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles/HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Our E-Commerce Site</h1>
-      {/* Other home page content */}
+    <div className="homepage">
+      <section className="hero">
+        <h1>Welcome to Our E-Commerce Site</h1>
+        <p>Find the best products for all your needs</p>
+        <Link to="/products" className="btn btn-primary">Shop Now</Link>
+      </section>
+            
     </div>
   );
 };
