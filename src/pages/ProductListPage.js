dc4d47454f517d99fbc2d51dacd62bf7de2d05d8
Marcel Mroz <mmroz.marcel@gmail.com>
2023-11-11 19:15:18 +0100

    product page

--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ProductCard from '../components/ProductCard';
+import '../styles/ProductListPage.css';
 
 const ProductListPage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
   return (
-    <div>
-      <h1>Product List</h1>
-      {/* Product listing logic */}
+    <div className="product-list-page">
+      <h1>Our Products</h1>
+      <div className="product-list">
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
