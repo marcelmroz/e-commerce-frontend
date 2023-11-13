7537bbc56905b9269d6136bbb0c0258913d76d57
Marcel Mroz <mmroz.marcel@gmail.com>
2023-11-13 19:27:53 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import RoutesComponent from './Routes';
+// import Header from './components/Header.js';
 import './App.css';
 
 const App = () => {
   return (
     <>
+      {/* <Header /> */}
       <RoutesComponent />
     </>
   );
