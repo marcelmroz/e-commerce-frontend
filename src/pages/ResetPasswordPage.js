27a5b104e2cf26a9d2fb198740767f5bb64699f9
Marcel Mroz <mmroz.marcel@gmail.com>
2023-12-09 20:54:13 +0100

    add resetpasswordpage

--- src/pages/ResetPasswordPage.js
+++ src/pages/ResetPasswordPage.js
@@ -1,25 +1,65 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const ResetPasswordPage = () => {
-  const [tempPassword, setTempPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [emailAddress, setEmailAddress] = useState('');
-  const [showNewPasswordForm, setShowNewPasswordForm] = useState(false);
-  const navigate = useNavigate();
+    const [tempPassword, setTempPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [showNewPasswordForm, setShowNewPasswordForm] = useState(false);
+    const [popupMessage, setPopupMessage] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  useState(() => {
-    if (location.state?.emailAddress) {
-      setEmailAddress(location.state.emailAddress);
+    useEffect(() => {
+        if (location.state?.emailAddress) {
+          setEmailAddress(location.state.emailAddress);
+        }
+      }, [location.state]);
+
+
+    const handleSendTempPasswordEmail = async () => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/customers/forgot-password?emailAddress=${emailAddress}`, {
+            method: 'POST'
+        });
+        if (response.ok) {
+            setPopupMessage('Temporary password email sent successfully.');
+        } else {
+            setPopupMessage('Failed to send temporary password email.');
+        }
+    } catch (error) {
+        console.error('Failed to send temporary password email:', error);
+        setPopupMessage('Error sending temporary password email.');
     }
-  }, [location.state]);
+    setTimeout(() => setPopupMessage(''), 5000);
+};
 
   const handleTempPasswordSubmit = async (event) => {
     event.preventDefault();
-    // Verify the temporary password here (you might need an API endpoint for this)
-    // If verification is successful:
-    setShowNewPasswordForm(true);
+    try {
+      const response = await fetch('http://localhost:8080/api/customers/verify-temp-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          emailAddress,
+          tempPassword,
+        }),
+      });
+
+      if (response.ok) {
+        setShowNewPasswordForm(true);
+      } else {
+        const message = await response.text();
+        setPopupMessage(message);
+        setTimeout(() => setPopupMessage(''), 5000);
+      }
+    } catch (error) {
+      console.error('Temporary password verification failed:', error);
+    }
   };
+
 
   const handleNewPasswordSubmit = async (event) => {
     event.preventDefault();
@@ -47,37 +87,41 @@ const ResetPasswordPage = () => {
 
   return (
     <div>
-      {!showNewPasswordForm ? (
-        <form onSubmit={handleTempPasswordSubmit}>
-          <h2>Enter Temporary Password</h2>
-          <input
-            type="email"
-            placeholder="Email Address"
-            value={emailAddress}
-            onChange={(e) => setEmailAddress(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Temporary Password"
-            value={tempPassword}
-            onChange={(e) => setTempPassword(e.target.value)}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      ) : (
-        <form onSubmit={handleNewPasswordSubmit}>
-          <h2>Set New Password</h2>
-          <input
-            type="password"
-            placeholder="New Password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-          />
-          <button type="submit">Update Password</button>
-        </form>
-      )}
+        {popupMessage && <div className="popup-message">{popupMessage}</div>}
+        {!showNewPasswordForm ? (
+            <>
+                <form onSubmit={handleTempPasswordSubmit}>
+                    <h2>Enter Temporary Password</h2>
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        value={emailAddress}
+                        onChange={(e) => setEmailAddress(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Temporary Password"
+                        value={tempPassword}
+                        onChange={(e) => setTempPassword(e.target.value)}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+                <button onClick={handleSendTempPasswordEmail}>Send Temporary Password Email</button>
+            </>
+        ) : (
+            <form onSubmit={handleNewPasswordSubmit}>
+                <h2>Set New Password</h2>
+                <input
+                    type="password"
+                    placeholder="New Password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <button type="submit">Update Password</button>
+            </form>
+        )}
     </div>
-  );
-};
+    );
+}
 
 export default ResetPasswordPage;
