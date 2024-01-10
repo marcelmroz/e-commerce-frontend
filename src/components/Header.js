f85a5ef5cb642193d4f8432a29360c9d84787649
Marcel Mroz <mmroz.marcel@gmail.com>
2024-01-10 18:53:46 +0100

    admin panel in header if role ADMIN

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,7 @@ import '../styles/Header.css';
 const Header = () => {
   const navigate = useNavigate();
   const isLoggedIn = sessionStorage.getItem('userToken') !== null;
-  const isAdmin = sessionStorage.getItem('userId') === '58';
-
+  const [isAdmin, setIsAdmin] = useState(false);
   const [cartCount, setCartCount] = useState(0);
 
   useEffect(() => {
@@ -14,12 +13,36 @@ const Header = () => {
       const cart = JSON.parse(sessionStorage.getItem('cart')) || [];
       setCartCount(cart.length);
     };
+  
+    const fetchUserRole = async () => {
+      if (isLoggedIn) {
+        try {
+          const response = await fetch('http://localhost:8080/api/customers/account', {
+            headers: {
+              'Authorization': 'Bearer ' + sessionStorage.getItem('userToken'),
+              'Content-Type': 'application/json',
+            },
+          });
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const customerDto = await response.json();
+          setIsAdmin(customerDto.role === 'ADMIN');
+        } catch (error) {
+          console.error('Error fetching user role:', error);
+        }
+      }
+    };
+  
     updateCartCount();
+    fetchUserRole();
+  
     window.addEventListener('cartUpdated', updateCartCount);
+  
     return () => {
       window.removeEventListener('cartUpdated', updateCartCount);
     };
-  }, []);
+  }, [isLoggedIn]);
 
   const handleLogout = () => {
     sessionStorage.clear();
@@ -35,22 +58,22 @@ const Header = () => {
         <nav className="navigation">
           <Link to="/" className="nav-link">Home</Link>
           {isLoggedIn ? (
-            <button onClick={handleLogout} className="nav-link header-btn right">
-              Logout
-            </button>
+            <>
+              <button onClick={handleLogout} className="nav-link header-btn right">
+                Logout
+              </button>
+              <Link to="/account" className="nav-link right">My account</Link>
+              {isAdmin && (
+                <Link to="/admin" className="nav-link right">Admin</Link>
+              )}
+            </>
           ) : (
             <>
               <Link to="/login" className="nav-link right">Log In</Link>
               <Link to="/register" className="nav-link right">Register</Link>
             </>
           )}
-          <Link to="/account" className="nav-link right">My account</Link>
-          {isAdmin ? (
-          <Link to="/admin" className="nav-link right">Admin</Link>
-          ) : null
-          }
-      <Link to="/cart" className="nav-link">My Cart ({cartCount})</Link>
-
+          <Link to="/cart" className="nav-link">My Cart ({cartCount})</Link>
         </nav>
       </div>
     </header>
